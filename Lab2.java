ccbc242bbd6440d5172bb776dcd5b3f529d61e10
Sam Clarke <SClarke4772@student.whatcom.edu>
2024-01-15 08:56:31 -0800

    added intro and menu

--- Lab2.java
+++ Lab2.java
@@ -2,6 +2,7 @@ public class Lab2
 {
     public static void main(String[] args)
     {
+        //Initialize, shuffle, and deal hands for two "players"
         DeckOfCards myDeckOfCards = new DeckOfCards();
         DeckOfCards mySecondDeckOfCards = new DeckOfCards();
         myDeckOfCards.shuffle();
@@ -9,6 +10,7 @@ public class Lab2
         myDeckOfCards.dealHand();
         mySecondDeckOfCards.dealHand();
         
+        //Print both hands
         System.out.println("First hand of cards:");
         for (int i = 0; i < 5; i++)
         {
@@ -20,6 +22,7 @@ public class Lab2
             System.out.printf("%-19s", (mySecondDeckOfCards.getHand())[i]);
         }
 
+        //Show results
         System.out.println("\nResults:");
         System.out.println(myDeckOfCards.compareHands(mySecondDeckOfCards));
     }
